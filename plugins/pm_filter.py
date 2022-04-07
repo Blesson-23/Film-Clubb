b9d80da5451aca9ae0d9dc3bf2bb9f0a1233ebf9
Hacker Jr <70173542+HackerX7@users.noreply.github.com>
2022-04-07 21:44:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -694,12 +694,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton(' 𝙾𝚠𝚗𝚎𝚛 ', url='https://t.me/albintko'),
             InlineKeyboardButton(' 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 ', url='https://t.me/+AMHw_K1wvOM3MTU9')
-        ]]
+        ]]        
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.reply_chat_action("typing")
-        m=await message.reply_sticker("CAACAgUAAxkBAAIIgGImFJuYfbGligABsx5NjNpCVdsdpQACwwIAArnFkFfGR5pt0kCs6h4E")
-        await asyncio.sleep(1)
-        await m.delete()
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
